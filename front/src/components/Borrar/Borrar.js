9391e77065cd02d2291b6b5533594b202629c6b9
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-20 16:37:54 -0500

    quite warning por poner useEffect como funcón asincrona y refactorizacion del metodo delete

--- front/src/components/Borrar/Borrar.js
+++ front/src/components/Borrar/Borrar.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
 import axios from 'axios'
-import futbol from '../../assets/image/futbol.jpg'
 
 
 export const Borrar = () => {
@@ -13,21 +12,26 @@ export const Borrar = () => {
     const [news, setnews] = useState({})
     const [img, setimg] = useState({})
 
-    useEffect(async () => {
+    useEffect(() => {
+      const fetchData = async () => {
         const res = await fetch(`http://localhost:3001/api/noticias/buscar/${id}`)
         const data = await res.json()
         const rest = await fetch(`http://localhost:3001/api/noticias/imagen/${id}`)
         const datas = await rest.json()
         setimg(datas[0].nombre)
         setnews(data)
+      }
+      fetchData()
+      // eslint-disable-next-line
     }, [])
 
     const handleClik = (e) => {
         console.log("borrando")
         let result = window.confirm("estas por borrar un dato definitivamente")
-        if (result == true) {
-            const res = axios.delete(`http://localhost:3001/api/noticias/borrar/${id}`)
-                .then(res => console.log(res));
+        if (result === true) {
+          axios
+            .delete(`http://localhost:3001/api/noticias/borrar/${id}`)
+            .then(res => console.log(res));
         }
         historia.push("/noticias/administracion")
 
