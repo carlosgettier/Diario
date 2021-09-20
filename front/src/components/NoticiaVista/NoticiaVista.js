c05f1f46f3fcc33a864b6bb7d8dac5075810e46b
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-20 14:37:40 -0500

    quite warning por imagen sin alt y por useEffect asincrono

--- front/src/components/NoticiaVista/NoticiaVista.js
+++ front/src/components/NoticiaVista/NoticiaVista.js
@@ -2,26 +2,29 @@ import React from 'react'
 import './noticiaVista.css'
 import { useParams } from 'react-router-dom'
 import { Propaganda } from '../Propaganda/Propaganda'
-import dolar from '../../assets/image/dolar.webp'
+// import dolar from '../../assets/image/dolar.webp'
 import { useState, useEffect } from 'react'
 
 export const NoticiaVista = () => {
+  const { id } = useParams()
+  
 
-    const { id } = useParams()
-
-    const [noticia, setNoticia] = useState({})
-
-    const [img, setimg] = useState({})
-
-    useEffect(async () => {
-        const res = await fetch(`http://localhost:3001/api/noticias/buscar/${id}`)
-        const data = await res.json()
-        const rest = await fetch(`http://localhost:3001/api/noticias/imagen/${id}`)
-        const datas = await rest.json()
-        setimg(datas[0].nombre)
-        setNoticia(data)
-        console.log(datas)
-
+  const [noticia, setNoticia] = useState({})
+  
+  const [img, setimg] = useState({})
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`http://localhost:3001/api/noticias/buscar/${id}`)
+      const data = await res.json()
+      const rest = await fetch(`http://localhost:3001/api/noticias/imagen/${id}`)
+      const datas = await rest.json()
+      setimg(datas[0].nombre)
+      setNoticia(data)
+      console.log(datas)
+    }
+    fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
 
@@ -32,7 +35,7 @@ export const NoticiaVista = () => {
                     <h5>Noticias {noticia.tipo}</h5>
                 </div >
                 <h1>{noticia.titulo}</h1>
-                <img src={'http://localhost:3001/' + img} />
+                <img src={'http://localhost:3001/' + img} alt={noticia.titulo} />
                 <p>{noticia.desarrollo}</p>
 
 
